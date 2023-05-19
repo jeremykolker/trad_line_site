ca54a4647b84efc91ac2935edf0e2dd45de30b00
“Jeremy <jeremy.kolker@gmail.com>
2023-05-19 14:48:38 -0400

    site restructure

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import ReactDOM from "react-dom/client";
+import App from './app/App';
+import './index.css';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <App />
+);
