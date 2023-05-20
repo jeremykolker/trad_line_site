d4d1288aa71b96f75a0db5fd55b836fb51af0352
“Jeremy <jeremy.kolker@gmail.com>
2023-05-20 17:10:59 -0400

    re routing

--- my-app/src/content_option.js
+++ my-app/src/content_option.js
@@ -1,11 +1,11 @@
-const logotext = "JOHN";
+const logotext = "Traditional Line";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Traditional Line",
+    description: "ARCHITECTURAL RESTORATION ",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "Traditional Line",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
